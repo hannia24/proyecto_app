cdd81f383f0fec497dfbac666ee9d5a6c7aa2a8b
Tu nombre <81984791+hannia24@users.noreply.github.com>
2022-02-23 20:15:06 -0500

    commit on master

---
+++ src/app/pages/prueba-page/prueba-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba-page',
+  templateUrl: './prueba-page.component.html',
+  styleUrls: ['./prueba-page.component.css']
+})
+export class PruebaPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
